6f44d677895391db5b92716268627bebbc98a9e2
Ramashka_AE <ramashka_ay@st.by>
2021-10-23 03:01:31 +0300

    add base structure and country catalog

---
+++ library/country/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+}
+
+version = "1.0"
+
+kotlin {
+    android()
+    ios()
+    jvm()
+    js()
+
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+    }
+}
+
+android {
+    compileSdkVersion(30)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(30)
+    }
+}
